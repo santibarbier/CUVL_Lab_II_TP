fe6b6ba642f26306ca04c7ae59e3314e83ad6a9c
fdz <fdzextra@gmail.com>
2019-07-08 12:32:58 -0300

    pupipo

--- menu_ventas.c
+++ menu_ventas.c
@@ -3,8 +3,11 @@
 #include "ayuda.h"
 #include "menu_ventas.h"
 
+// static Nodo* pCarrito;
+
 void menuVentas()
 {
+    // static Nodo* pCarrito;
     limpiarPantalla();
     printf("menuVentas()\n");
     presioneUnaTeclaParaContinuar();
